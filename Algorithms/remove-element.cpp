18012bcef9723cf7708536964e14015cf4ada5e4
Shuvo Karmakar <shuvosksk@gmail.com>
2016-12-21 04:27:29 +0600

    Create remove-element.cpp

---
+++ Algorithms/remove-element.cpp
@@ -0,0 +1,11 @@
+//Problem Description: https://leetcode.com/problems/remove-element
+class Solution 
+{
+public:
+    int removeElement(vector<int>& nums, int val) 
+    {
+        for(int i = 0; i < nums.size(); i++)
+            if(nums[ i ] == val) nums.erase(nums.begin() + i--);
+        return nums.size();
+    }
+};
