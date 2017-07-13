b24b29779de34c91e86a8dd9234477a9230c5178
Shuvo Karmakar <shuvosksk@gmail.com>
2017-07-14 02:39:32 +0600

    Create valid-palindrome.c

---
+++ Algorithms/valid-palindrome.c
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/valid-palindrome/#/description
+bool isPalindrome(char* s) 
+{
+    int l=0, r = strlen(s)-1;
+    while(l <= r)
+    {
+        if(!isalpha(s[l]) && !isdigit(s[l])) l++;
+        else if(!isalpha(s[r]) && !isdigit(s[r])) r--;
+        else
+        {
+            if(tolower(s[l]) != tolower(s[r])) return false;
+            l++; r--;
+        }
+    }
+    return true;
+}
