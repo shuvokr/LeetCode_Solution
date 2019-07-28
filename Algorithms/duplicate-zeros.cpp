83740e1d0bba714f3ebf6e741fd51e56ec02cae9
Shuvo Karmakar <12962997+shuvokr@users.noreply.github.com>
2019-07-28 15:44:42 +0600

    Create duplicate-zeros.cpp

---
+++ Algorithms/duplicate-zeros.cpp
@@ -0,0 +1,19 @@
+// Link : https://leetcode.com/problems/duplicate-zeros
+
+class Solution {
+public:
+    int bitwiseComplement(int N) {
+        if(!N) return 1;
+        vector<int> binary;
+        while(N) {
+            binary.push_back(N&1);
+            N >>= 1;
+        }
+        for(int i = 0; i < binary.size(); i++)
+            binary[i] = binary[i] == 0 ? 1 : 0;
+        long long ret = 0LL, val = 1LL;
+        for(int i = 0; i < binary.size(); i++)
+            ret += val * binary[i], val <<= 1;
+        return ret;
+    }
+};
