7460677d91de3ec695c9d806841e8dfa99cd4bce
Shuvo Karmakar <12962997+shuvokr@users.noreply.github.com>
2019-08-21 10:38:15 +0600

    Create valid-phone-numbers.cpp

---
+++ Algorithms/valid-phone-numbers.cpp
@@ -0,0 +1,4 @@
+// Link : https://leetcode.com/problems/valid-phone-numbers
+
+# Read from the file file.txt and output all valid phone numbers to stdout.
+awk '/^([0-9]{3}-|\([0-9]{3}\) )[0-9]{3}-[0-9]{4}$/' file.txt
